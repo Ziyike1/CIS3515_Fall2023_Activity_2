e901f2e0662945bdf988aef0b66a0a1fe05dde2b
Ziyike1 <tul51428@temple.edu>
2023-09-19 12:08:01 -0400

    finish step2 and step3 about create adapter to display items and change text size

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -15,19 +15,31 @@ class MainActivity : AppCompatActivity() {
 
         /* Step 1: Populate this array */
         //val numberArray = Array Of Numbers
-        val sizeArray =IntArray(100)
-        for (x in sizeArray){
-            for (element in sizeArray){
-                println(element)
-            }
-        }
+        val sizeArray = (1..100).toList()
 
         /* Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter..
+        spinner.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,sizeArray)
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selected = sizeArray[position]
+                displayTextView.textSize = selected.toFloat()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
 
     }
 }
